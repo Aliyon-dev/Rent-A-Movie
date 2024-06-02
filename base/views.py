684cc4182257c92e97c27e70c53537bfd97b0698
Aliyon-dev <temboaliyon@gmail.com>
2024-06-02 22:00:12 +0200

    added html templates

--- base/views.py
+++ base/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+# function to render the home page
 def home(request):
-    return HttpResponse("testing the project")
-    
+    return render(request, 'base.html')
+
+
+
+#function to handle login from the user
+def login(request):
+    return HttpResponse("Login")
