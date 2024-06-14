78f21358a84c7d294e7964dac1d97b5aee954814
Mulenga Musamba <walkermule7@gmail.com>
2024-06-14 13:42:34 +0200

    created customer details view and update and delete view see if its working correctly

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, login, customers, movies, add_customer, test, add_movie, get_genres, crud_movie
+from .views import delete_customer, get_customerDetails, home, login, customers, movies, add_customer, test, add_movie, get_genres, crud_movie, update_customer
 
 urlpatterns = [
     path('', home, name="home"),
@@ -11,5 +11,8 @@ urlpatterns = [
     path('get_genres', get_genres, name="get_genres"),
     path('get-movie/<int:key>', crud_movie, name="get-movie"),
     path('get-movie/<str:key>', crud_movie, name="get-movie"),
-    path('modal', test)
+    path('modal', test),
+    path('customer/<int:customer_id>/', get_customerDetails, name='customer_detail'), 
+    path('customer/<int:customer_id>/update/', update_customer, name='update_customer'),
+    path('customer/<int:customer_id>/delete/', delete_customer, name='delete_customer'),
 ]
